9ebd09ab9c47985145ff929e4620fb51df1c3ea9
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-05-13 13:39:39 -0300

    WhatsApp - Criando interface de login

--- WhatsApp/app/src/main/java/com/example/whatsapp/MainActivity.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void textCadastrar(View v){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
 }
