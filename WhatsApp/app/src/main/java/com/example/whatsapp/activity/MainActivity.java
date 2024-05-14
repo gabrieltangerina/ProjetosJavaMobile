6167601e8c6a9e3bd9f7770bdab34bf392f7297a
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-05-14 10:27:05 -0300

    WhatsApp - Configurando itens de menu

--- WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
@@ -1,22 +1,60 @@
 package com.example.whatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.whatsapp.R;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar); // Suporte a versões anteriores do android
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Verifica qual item de menu foi clicado
+
+        if(item.getItemId() == R.id.menu_sair){
+            deslogarUsuario();
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+        try{
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
