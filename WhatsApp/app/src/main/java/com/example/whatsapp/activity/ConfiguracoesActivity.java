7d91b88547e30e43ecaff6a97cce312bbbd52886
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-05-16 13:51:29 -0300

    WhatsApp - Configurando abrir camera

--- WhatsApp/app/src/main/java/com/example/whatsapp/activity/ConfiguracoesActivity.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/activity/ConfiguracoesActivity.java
@@ -4,21 +4,34 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.whatsapp.R;
 import com.example.whatsapp.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
-    public String[] permissoesNecessarios = new String[]{
-            Manifest.permission.READ_EXTERNAL_STORAGE,
+    private String[] permissoesNecessarios = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+
+    private ImageButton imageButtonCamera, imageButtonGaleria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +39,30 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuracoes);
 
         // Validando permissões
-        Permissao.validarPermissoes(permissoesNecessarios, this, 1); // O requestCode é para saber de onde veio a requisição
-        alertaValidacaoPermissao();
+        // Permissao.validarPermissoes(permissoesNecessarios, this, 1);
+        // alertaValidacaoPermissao();
 
         // Config. Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Config. Seta de Voltar
 
+        // Config. Icones Foto Perfil
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_CAMERA);
+                }
+
+            }
+        });
     }
 
     @Override
@@ -45,16 +72,15 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         // Tratando a negação da permissão das solicitações
         for(int permissaoResultado : grantResults){
             if(permissaoResultado == PackageManager.PERMISSION_DENIED){
-
+                alertaValidacaoPermissao();
             }
         }
-
     }
 
     private void alertaValidacaoPermissao(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Permissões Negadas");
-        builder.setCancelable(false); // Para n fechar o alert ao clicar fora dele
+        builder.setCancelable(false); // Para não fechar o alert ao clicar fora dele
         builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
 
         builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
--- WhatsApp/app/src/main/java/com/example/whatsapp/helper/Permissao.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/helper/Permissao.java
@@ -3,7 +3,6 @@ package com.example.whatsapp.helper;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
@@ -15,20 +14,23 @@ import java.util.List;
 public class Permissao {
     public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode){
 
-        List<String> listaPermissoesNaoAutorizadas = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            List<String> listaPermissoesNaoAutorizadas = new ArrayList<>();
 
-        for(String permissao: permissoes){
-            boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+            for(String permissao : permissoes){
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                Toast.makeText(activity, permissao + " | " + temPermissao, Toast.LENGTH_SHORT).show();
+                if(!temPermissao) listaPermissoesNaoAutorizadas.add(permissao);
+            }
 
-            if(!temPermissao) listaPermissoesNaoAutorizadas.add(permissao);
+
+            if(listaPermissoesNaoAutorizadas.isEmpty()) return true;
+
+
+            String[] novasPermissoes = new String[listaPermissoesNaoAutorizadas.size()];
+            listaPermissoesNaoAutorizadas.toArray(novasPermissoes);
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
         }
-
-        if(listaPermissoesNaoAutorizadas.isEmpty()) return true;
-
-        String[] novasPermissoes = new String[listaPermissoesNaoAutorizadas.size()];
-        listaPermissoesNaoAutorizadas.toArray(novasPermissoes);
-
-        ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
 
         return true;
     }
