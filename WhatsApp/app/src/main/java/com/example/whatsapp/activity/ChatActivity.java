2d0b1e90d588a4d379ba8243e32951bef9538bce
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-05-18 22:30:41 -0300

    WhatsApp - Salvando mensagens no Firebase

--- WhatsApp/app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
+++ WhatsApp/app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
@@ -2,27 +2,25 @@ package com.example.whatsapp.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
-
-import com.bumptech.glide.Glide;
-import com.example.whatsapp.model.Usuario;
-import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toolbar;
-
 import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.whatsapp.databinding.ActivityChatBinding;
-
+import com.bumptech.glide.Glide;
 import com.example.whatsapp.R;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
+import com.example.whatsapp.databinding.ActivityChatBinding;
+import com.example.whatsapp.helper.Base64Custom;
+import com.example.whatsapp.helper.UsuarioFirebase;
+import com.example.whatsapp.model.Mensagem;
+import com.example.whatsapp.model.Usuario;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,9 +29,12 @@ public class ChatActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityChatBinding binding;
     private Usuario usuarioDestino;
+    private String idUsuarioDestinatario;
+    private String idUsuarioRemetente;
 
     private CircleImageView circleImageViewFotoChat;
     private TextView textViewNomeChat;
+    private EditText editMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,25 +50,51 @@ public class ChatActivity extends AppCompatActivity {
 
         textViewNomeChat = findViewById(R.id.textViewNomeChat);
         circleImageViewFotoChat = findViewById(R.id.circleImageFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
 
         // Recuperar os dados passados no onClick de ContatosFragment
         Bundle bundle = getIntent().getExtras();
-
-        if(bundle != null){
+        if (bundle != null) {
             usuarioDestino = (Usuario) bundle.getSerializable("chatContato");
             textViewNomeChat.setText(usuarioDestino.getNome());
             String foto = usuarioDestino.getFoto();
-
-            if(foto != null){
+            if (foto != null) {
                 Uri url = Uri.parse(foto);
                 Glide.with(ChatActivity.this)
                         .load(url)
                         .into(circleImageViewFotoChat);
-            }else{
+            } else {
                 circleImageViewFotoChat.setImageResource(R.drawable.padrao);
             }
         }
 
+        // Recupera dados do usuário remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdUser();
+
+        // Recupera dados do usuário destinatário
+        idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestino.getEmail());
     }
 
+    public void enviarMensagem(View view) {
+        String textoMensagem = editMensagem.getText().toString();
+        if (!textoMensagem.isEmpty()) {
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdRemetente(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+            editMensagem.setText(""); // Limpar o campo de mensagem após enviar
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem mensagem) {
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+        mensagemRef
+                .child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(mensagem);
+
+        editMensagem.setText("");
+    }
 }
---
+++ WhatsApp/app/src/main/java/com/example/whatsapp/model/Mensagem.java
@@ -0,0 +1,34 @@
+package com.example.whatsapp.model;
+
+public class Mensagem {
+    private String idRemetente;
+    private String mensagem;
+    private String imagem;
+
+    public Mensagem() {
+    }
+
+    public String getIdRemetente() {
+        return idRemetente;
+    }
+
+    public void setIdRemetente(String idRemetente) {
+        this.idRemetente = idRemetente;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+}
